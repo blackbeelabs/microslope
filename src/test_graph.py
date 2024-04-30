8426dc33561221b6d4e610bd37dcf8365ffb4a15
blackbeelabs <blackbeelabs@outlook.com>
2024-04-30 14:38:48 +0800

    add test

--- src/test_graph.py
+++ src/test_graph.py
@@ -102,41 +102,22 @@ def test_relu():
     assert c._op == "relu"
 
 
-def trace(root):
-    # builds a set of all nodes and edges in a graph
-    nodes, edges = set(), set()
-
-    def build(v):
-        if v not in nodes:
-            nodes.add(v)
-            for child in v._prev:
-                edges.add((child, v))
-                build(child)
-
-    build(root)
-    return nodes, edges
+def test_times_minus_one():
+    a = Node(3)
+    c = a.times_minus_one()
+    assert c.data == -3
+    assert c._op == "*-1"
 
 
-def draw_dot(root):
-    dot = Digraph(format="svg", graph_attr={"rankdir": "LR"})  # LR = left to right
+def test_plus_one():
+    a = Node(3)
+    c = a.plus_one()
+    assert c.data == 4
+    assert c._op == "+1"
 
-    nodes, edges = trace(root)
-    for n in nodes:
-        uid = str(id(n))
-        # for any value in the graph, create a rectangular ('record') node for it
-        dot.node(
-            name=uid,
-            label="{ %s | data %.4f | grad %.4f }" % (n.label, n.data, n.grad),
-            shape="record",
-        )
-        if n._op:
-            # if this value is a result of some operation, create an op node for it
-            dot.node(name=uid + n._op, label=n._op)
-            # and connect this node to it
-            dot.edge(uid + n._op, uid)
 
-    for n1, n2 in edges:
-        # connect n1 to the op node of n2
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-
-    return dot
+def test_reciprocal():
+    a = Node(3)
+    c = a.reciprocal()
+    assert c.data == 1 / 3
+    assert c._op == "1/x"
